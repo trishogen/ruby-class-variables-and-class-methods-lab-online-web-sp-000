1a3f43d6c804b05912215a04c170b453cbc854fa
Trish Ogen <patriciaogen@gmail.com>
2019-05-26 02:22:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,6 +4,7 @@ class Song
 
   @@count = 0
   @@artists = []
+  @@genres = []
 
   def initialize(name, artist, genre)
     @name = name
@@ -12,6 +13,7 @@ class Song
 
     @@count +=1
     @@artists << artist
+    @@genres << genre
   end
 
 end
